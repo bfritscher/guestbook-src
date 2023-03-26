51403c649277b3125871eac786354b8ef3f1989f
Boris Fritscher <boris@fritscher.ch>
2023-03-26 22:26:20 +0200

    java firestore fix compose

--- 05_gcp_datastore_java/src/main/java/ch/hegarc/guestbook/MainController.java
+++ 05_gcp_datastore_java/src/main/java/ch/hegarc/guestbook/MainController.java
@@ -4,7 +4,6 @@ import ch.hegarc.guestbook.models.Entry;
 import ch.hegarc.guestbook.services.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
