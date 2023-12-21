8a751c17fe1304b8da09ad07181c539d0e03f04a
Boris Fritscher <boris@fritscher.ch>
2023-12-21 12:52:50 +0100

    show python footer in all versions

--- 02_mvp_modules_sqlite3/index.py
+++ 02_mvp_modules_sqlite3/index.py
@@ -20,4 +20,4 @@ class Index(MethodView):
     def get(self):
         model = gbmodel.get_model()
         entries = [dict(name=row[0], email=row[1], signed_on=row[2], message=row[3] ) for row in model.select()]
-        return render_template('index.html',entries=entries, hostname=socket.gethostname(), model_name=model.__class__.__name__, ip=get_ip())
+        return render_template('index.html', entries=entries, hostname=socket.gethostname(), model_name=model.__class__.__name__, ip=get_ip())
